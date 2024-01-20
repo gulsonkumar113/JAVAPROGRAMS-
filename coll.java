79c7672813b0055843b3368c3a0b89ecdc56d769
GULSON KUMAR <gulsonkumar113@gmail.com>
2024-01-20 12:18:28 +0530

    collections

---
+++ coll.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class coll {
+    public static void main(String[] args) {
+        
+       ArrayList ar= new ArrayList();
+       ar.add("gkr ");
+       System.out.println(ar.add(56));
+
+       System.out.println(ar);
+       System.out.println(ar.remove(1));
+       System.out.println(ar.get);
+    }
+}
